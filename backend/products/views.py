3bcfe9e7dd90ae2a5a62cb62fa7b6a48d6ec51db
VanV <vainavinair2009558@gmail.com>
2023-04-14 23:30:23 +0530

    implemented mixins

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from django.shortcuts import get_object_or_404
 # from django.http import Http404
@@ -78,3 +78,33 @@ class ProductListAPIView(generics.ListAPIView):
 #         return Response(serializer.data)
 
     
+
+class ProductMixinView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+    ):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs): #http -> get
+        print(args,kwargs)
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs): #http -> post
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
--- py_client/create.py
+++ py_client/create.py
@@ -4,7 +4,6 @@ endpoint = "http://localhost:8000/api/products/"
 
 data = {
     'title':'Speaker',
-    'content':'this is a speaker',
     'price':18.99
 }
 get_response=requests.post(endpoint,data) 
