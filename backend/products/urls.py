dc69053ead3f8cb87be7960e5048a2167ea8b013
VanV <vainavinair2009558@gmail.com>
2023-04-16 22:49:33 +0530

    added reverse urls

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views as product_views
 
 urlpatterns = [
-    path('<int:pk>/', product_views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/update', product_views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/', product_views.ProductDetailAPIView.as_view(), name="product-detail"),
+    path('<int:pk>/update', product_views.ProductUpdateAPIView.as_view(), name="product-edit"),
     path('<int:pk>/delete', product_views.ProductDeleteAPIView.as_view()),
-    path('', product_views.ProductListCreateAPIView.as_view()),
+    path('', product_views.ProductListCreateAPIView.as_view(), name="product-list"),
 ]
